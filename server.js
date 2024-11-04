81fed4be2857246384dff2b5734cad85606a78f8
abdulfortech <ibnmudi@gmail.com>
2024-11-04 07:21:20 +0100

    server

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const contactsRouter = require('./routes/contactRoutes.js');
+const errorHandler = require('./middleware/errorHandler.js');
+
+// // Use the router
+// app.use('/contacts', contactsRouter);
 
 const app = express();
 
 const port = process.env.PORT || 5670;
+app.use(express.json());
 
-
-app.get('/api/contacts', (req, res) => {
-    res.send('get all contacts')
-});
+app.use("/api/contacts", require('./routes/contactRoutes'));
+app.use(errorHandler);
+// app.get();
 
 app.listen(port, () => {
     console.log(`server started on port ${port}`);
